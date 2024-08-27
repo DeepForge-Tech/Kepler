174c50cc08b4f8047a32fcc778d03a8293bbbdd1
blackflame576 <pws15548@gmail.com>
2024-08-27 18:27:24 +0300

    Add support of shared build

--- cmake/DownloadDependecies.cmake
+++ cmake/DownloadDependecies.cmake
@@ -14,39 +14,39 @@ FetchContent_Declare(Logger
 FetchContent_MakeAvailable(Logger)
 
 # fmt
-FetchContent_Declare(fmt
-  GIT_REPOSITORY https://github.com/DeepForge-Technology/fmt.git
-  GIT_TAG master
-  GIT_SHALLOW 1
-)
+# FetchContent_Declare(fmt
+#   GIT_REPOSITORY https://github.com/DeepForge-Technology/fmt.git
+#   GIT_TAG master
+#   GIT_SHALLOW 1
+# )
 # if(NOT fmt_POPULATED)
 #   FetchContent_Populate(fmt)
 
 #   add_subdirectory(${fmt_SOURCE_DIR} ${fmt_BINARY_DIR} EXCLUDE_FROM_ALL)
 # endif()
-FetchContent_MakeAvailable(fmt)
+# FetchContent_MakeAvailable(fmt)
 
 # cppcoro
-FetchContent_Declare(cppcoro
-  GIT_REPOSITORY https://github.com/DeepForge-Tech/cppcoro.git
-  GIT_TAG main
-  GIT_SHALLOW 1
-)
+# FetchContent_Declare(cppcoro
+#   GIT_REPOSITORY https://github.com/DeepForge-Tech/cppcoro.git
+#   GIT_TAG main
+#   GIT_SHALLOW 1
+# )
 # if(NOT fmt_POPULATED)
 #   FetchContent_Populate(fmt)
 
 #   add_subdirectory(${fmt_SOURCE_DIR} ${fmt_BINARY_DIR} EXCLUDE_FROM_ALL)
 # endif()
-FetchContent_MakeAvailable(cppcoro)
+# FetchContent_MakeAvailable(cppcoro)
 
  # jsoncpp
- FetchContent_Declare(jsoncpp
- GIT_REPOSITORY https://github.com/DeepForge-Technology/jsoncpp.git
- GIT_TAG master
-)
+#  FetchContent_Declare(jsoncpp
+#  GIT_REPOSITORY https://github.com/DeepForge-Technology/jsoncpp.git
+#  GIT_TAG master
+# )
 # if (NOT jsoncpp_POPULATED)
 #   FetchContent_Populate(jsoncpp)
 #   add_subdirectory(${jsoncpp_SOURCE_DIR} ${jsoncpp_BINARY_DIR})
 # endif ()
-FetchContent_MakeAvailable(jsoncpp)
+# FetchContent_MakeAvailable(jsoncpp)
 
